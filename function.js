6c3c2598bbe955becd932e3960671f66b240d971
Alexana7 <ngayane@bk.ru>
2022-11-25 13:54:57 +0300

    new commit

--- function.js
+++ function.js
@@ -1,5 +1,56 @@
 // ex.6.1
-let price = 80000;
-(function priceMessage (price) {
-    console.log(`Данный товар стоит ${price} рублей`);
-})(price);
+// let price = 80000;
+// (function priceMessage (price) {
+//     console.log(`Данный товар стоит ${price} рублей`);
+// })(price);
+
+
+// ex.6.2
+// const num_1 = 3;
+// const num_2 = 6;
+
+// function mult (arg1, arg2) {
+//     return arg1 * arg2
+// }
+// console.log(mult(num_1, num_2));
+
+// //ex.6.3
+// const num = 3;
+// function square() {
+//     return num * num;
+// }
+// console.log(square())
+
+// ex.6.4
+// let num = 3;
+// let res = "";
+// let i = 0;
+// while (i < 3){
+//     num = Math.pow(num, 2);
+//     res = res.trim() + " " + num;
+//     i += 1;
+// }
+//  console.log(res);
+
+
+// ex.6.5
+// const string = "px";
+// function getNumber(data){
+    
+//     if (isNaN(parseInt(data))) {
+//         return 0;
+//     }
+//     return parseInt(data);
+// }
+// console.log(getNumber(string));
+
+// ex.6.6
+
+// const value = 7;
+// const total = 10;
+
+// function getPercent (a, b) {
+//    const result = a / b * 100;
+//    return result;
+// }
+// console.log(getPercent(value, total));
