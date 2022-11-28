9795225d4a00b65ce54d65dee92be11c9f82731a
Alexana7 <ngayane@bk.ru>
2022-11-28 21:45:52 +0300

    add arrays tasks

--- function.js
+++ function.js
@@ -54,3 +54,38 @@
 //    return result;
 // }
 // console.log(getPercent(value, total));
+
+
+// ************ARRAYS***************
+// *********************************
+
+// let arr = [1, 2, 3];
+// console.log(arr[-1]);
+// console.log(arr[9999]);
+// console.log(arr['Hello']);
+
+// let obj = {1: "3"};
+// console.log(obj[-1]);
+// console.log(obj[9999]);
+// console.log(obj['Hello']);
+
+// let arr1 = [4, 5, 6];
+// let arr2 = arr1;
+// arr2[0] = 2;
+// console.log(arr1)
+
+// let array = [4, 5, 6];
+// function f1(arr) {
+//     arr[0] = 'Ooops!';
+// }
+// f1(array);
+// console.log(array);
+
+// ex.9.1
+
+const list = ["Груша", "Банан", "Апельсин", "Виноград"];
+
+list.unshift("Яблоко");
+list.pop();
+list.push("Клубника");
+console.log(list);
