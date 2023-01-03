dbf452f226efaa920bec2a81a107560406a2bb02
Alexana7 <ngayane@bk.ru>
2023-01-03 12:48:32 +0300

    add reg exp tasks

--- js2.js
+++ js2.js
@@ -10,20 +10,24 @@
 
 // ex.1.3
 
-// const firstDiv = "<div><p>Первый заголовок</p></div>";
-
-// if (firstDiv.includes('<p>') && firstDiv.includes('</p>')) {
-//     console.log(true)
+// const firstDiv = "<div><p>Первый заголовок</p>p>Первый заголовок</p></div>";
+// if (firstDiv.includes('<p>')) {
+//     let n = firstDiv.indexOf('<p>');
+//     let counter = firstDiv.indexOf('<p>', n+1);
+//     counter <= 0 ? console.log(false) : console.log(true);
 // } else {
 //     console.log(false)
 // }
-
 // ex.1.4
 // let road = "15 km"
 // const findIndex = road.indexOf('km');
 // let roadMiles = road.slice(0, findIndex).trim();
 // roadMiles = parseFloat(roadMiles * 0.62).toFixed(1);
 // console.log(roadMiles);
+
+// ************
+// let length = +road.split(" ")[0];
+// console.log((length * 0.62).toFixed(1))
 
 // ex.1.5
 
@@ -45,3 +49,12 @@
 // let num = symbols.codePointAt(0) + symbols.codePointAt(1);
 // console.log(num)
 // num = (num % 2 === 0) ? console.log(String.fromCodePoint(num)) : console.log("Символ обнаружить не удалось"); 
+
+// ex.1.9
+// v.1
+// emails = "example@ex.ru primer@primer.com email@com.ru";
+// // v.2
+// let reg = /[a-z0-9-]+@[a-z]+\.[a-z]{2,3}/gi;
+// let result = emails.match(reg);
+// console.log(result || ['']);
+
